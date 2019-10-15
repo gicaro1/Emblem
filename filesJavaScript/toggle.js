eb797fa39a2d4195c4d49f243ee2d94946a15beb
gcarde01 <giocardenas2@icloud.com>
2019-10-15 20:01:03 +0100

    gio prueba

---
+++ filesJavaScript/toggle.js
@@ -0,0 +1,17 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+  var x = document.getElementById("burger");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.location.reload();
+}
+
+
+
+
+
+
+
