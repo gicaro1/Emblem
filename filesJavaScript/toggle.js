e5bfcfee7beeaad5a8f13d06c95cea606aa295e8
gcarde01 <giocardenas2@icloud.com>
2019-10-22 22:02:54 +0100

    first mobile responsive

--- filesJavaScript/toggle.js
+++ filesJavaScript/toggle.js
@@ -1,12 +1,15 @@
 function myFunction(x) {
-  x.classList.toggle("change");
+   x.classList.toggle("change");
   var x = document.getElementById("burger");
   if (x.style.display === "none") {
     x.style.display = "block";
+    
   } else {
     x.style.display = "none";
   }
+ 
   x.location.reload();
+  
 }
 
 
